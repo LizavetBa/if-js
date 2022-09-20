bacb3c42e63db89cc4057d163bde3c2b0a393bb2
LizavetBa <vetanikis@gmail.com>
2022-09-20 13:09:06 +0200

    added variables: user, student, test, result, arr2,arr3 and done some calculations ended home tasks

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ console.log(typeof test); //result:number;
 const example = 10;
 let z = 2;
 z++;
-console.log(example % z);//result: 1;
+console.log(example % z); //result: 1;
 
 //_____________________________
 
@@ -31,13 +31,13 @@ const k = 11;
 
 if (a === k) {
     console.log('done');
-} else console.log('try again');//result: try again;
+} else console.log('try again'); //result: try again;
 
 //_____________________________
 
-const man = {name: 'Joe', age: '32'};
+const man = { name: 'Joe', age: '32' };
 const status = man.age <= 40 ? 'old' : 'young';
-console.log(status);//result: old;
+console.log(status); //result: old;
 
 //_____________________________
 
@@ -49,7 +49,6 @@ for (let i = 0; i < arr.length; i++) {
 console.log(result); //result: 240
 //_______________________________
 
-
 const arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i < arr2.length; i++) {
     if (arr2[i] > 5 && arr2[i] < 10) {
@@ -60,10 +59,25 @@ for (let i = 0; i < arr2.length; i++) {
 //_______________________________
 
 const arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
-for (let i = 0; i < arr3.length; i++){
-    if(arr3[i] % 2 === 0){
+for (let i = 0; i < arr3.length; i++) {
+    if (arr3[i] % 2 === 0) {
         console.log(arr3[i]);
     }
 }
 //result:2,8,0,6,20
 //_________________________________
+
+function calc(n1, n2) {
+    console.log(n1 * n2);
+}
+
+const x = 4;
+const y = 6;
+
+calc(x, y);
+
+const square = function (number) {
+    console.log(number * number);
+};
+square(y);
+square(x);
