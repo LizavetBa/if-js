85ffbe0c7022f222cbe3eb62d2eb569ef2b1d496
LizavetBa <vetanikis@gmail.com>
2022-12-21 17:50:54 +0100

    first part is for a change date code second is for findHotel code

---
+++ src/scripts/main.js
@@ -0,0 +1,58 @@
+
+const changeDate = (date)=>{
+  return date.split('-').reverse().join('.')
+}
+console.log(changeDate('2020-11-26'));
+// _________________________
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
+
+function findhotel(date, element){
+  return data.filter(i => {
+    if(i.country === element || i.city === element || i.hotel === element) {
+      return i
+    }
+  })
+}
+
+console.log(findhotel(data, 'Bali'));
